c3d30568a3740d4de6d88e4d266ae0aeb09d502f
Kellie Henderson <kellie.henderson@my.tccd.edu>
2019-11-07 20:31:50 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/*
+	Input Validation with Ref Functions 
+	A cleaner way to do input validation
+	Validating input, an essential process.
+	Source: http://stackoverflow.com/questions/514420/how-to-validate-numeric-input-c
+	Keeping Dr. McMillan from breaking my code, defend against "dark side of the force",
+	and/or legitimate user error. 
+	//Try and Catch blocks are used in error checking
+	Teacher: Dr. Tyson McMillan 
+*/
+#include <cstdlib>  // Provides EXIT_SUCCESS
+#include <iostream>  // Provides cout, cerr, endl
+#include "Input_Validation_Extended.h" // Provides getValidatedInput<T>(), a header File with a Template Class
+                              // Extended 2-11-2016 by Dr. McMillan to add validation methods by datatype
+#include<string> //for string manipulation
+#include<sstream> //for string manipulation and comparison
+using namespace std;
+int main()
+{
+int n = 0;
+for (int n = 0; n <= 20; n++)
+{
+cout << "Looping again" << endl;
+
+}
+
+return 0;
+}
